29adfdacc3ba1ad735954e814b685685282c1b9f
Sebastien St Vil <kseb0@hotmail.com>
2022-05-07 23:12:01 -0400

    bug fixes

--- mastermindweb/blog_posts/forms.py
+++ mastermindweb/blog_posts/forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
+from flask_ckeditor import CKEditor, CKEditorField
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+from mastermindweb import app
 
+ckeditor = CKEditor(app)
 
 class BlogPostForm(FlaskForm):
     # no empty titles or text possible
     # we'll grab the date automatically from the Model later
     title = StringField('Title', validators=[DataRequired()])
-    text = TextAreaField('Text', validators=[DataRequired()])
-    submit = SubmitField('BlogPost')
+    text = CKEditorField('Text', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- mastermindweb/blog_posts/views.py
+++ mastermindweb/blog_posts/views.py
@@ -60,10 +60,13 @@ def update(blog_post_id):
                            form=form)
 
 
-@blog_posts.route("/<int:blog_post_id>/delete", methods=['POST'])
+@blog_posts.route("/<int:blog_post_id>/delete", methods=['GET', 'POST'])
 @login_required
 def delete_post(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
+
+    print(f"blogpost author: {blog_post.author}      current_user: {current_user}")
+
     if blog_post.author != current_user:
         abort(403)
     db.session.delete(blog_post)
